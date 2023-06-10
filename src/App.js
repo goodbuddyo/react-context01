c1e2c85b740e051ce8c94a617f63a014eca9f69d
Seano <sean01234@yahoo.com>
2023-06-10 19:02:42 -0400

    update footer etc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import {useState} from "react";
+//import ToDoListWithToolbar from "./components/todo/ToDoListWithToolbar";
+//import {TodosDataProvider} from "./contexts/ToDosDataContext";
+//import ToDoManager from "./components/todo/ToDoManager";
 import Layout from "./components/layout/Layout";
 
 const App=() => {
+  //const [displayStatus,setDisplayStatus]=useState("all"); // all, pending, completed
+  //const [important,setImportant]=useState(false);
+  //const [searchText,setSearchText]=useState("");
   const [darkTheme,setDarkTheme]=useState(false);
   const toggleTheme=() => setDarkTheme(!darkTheme);
   return (
-    <Layout toggleTheme={toggleTheme} darkTheme={darkTheme} />
+
+    <Layout toggleTheme={toggleTheme} darkTheme={darkTheme}>
+
+    </Layout>
+
   );
 };
 export default App;
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,26 +1,55 @@
+//import { useContext } from "react";
+//import { ToDosDataContext } from "../../contexts/ToDosDataContext";
 
 const Footer=() => {
+  const {todoList,deleteTodo,reFetch,loadingStatus}=
+    "lorum ipsum";
+  //useContext(ToDosDataContext)
+
+  const totalItems=todoList?.length;
+  const notCompletedItems=todoList?.filter(
+    (r) => !r.completed,
+  ).length;
+  const importantItems=todoList?.filter(
+    (r) => !r.completed&&r.important,
+  ).length;
+
+  const handleClearCompleted=() => {
+    const response=window.confirm("Clear Completed Todos?");
+    if(response) {
+      let completedIds=todoList
+      //  .filter((todoItem) => {
+      //    return todoItem.completed;
+      //  })
+      //  .map((rec) => rec.id);
+      //deleteTodo(completedIds);
+    }
+  };
+
   return (
     <footer className="text-center">
       <div className="stats">
         <button
           title="Refresh List"
           className="btn btn-theme-primary"
-          onClick={console.log("Refresh from footer")}
+          onClick={() => {
+            console.log('reFetch')
+          }}
         >
           <i className="fas fa-sync"></i>
         </button>
 
         <div className="clear-completed">
           <button
+            onClick={handleClearCompleted}
             className="btn btn-theme-danger btn-md"
-            onClick={console.log("Clear from footer")}
           >
             Clear Completed
           </button>
         </div>
 
         <div className="quick-stats">
+
           <p>
             <span className="badge text-bg-secondary">
               Badge Lorem ipsum
